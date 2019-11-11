46e2249758fb39e14830c30675cadbfedf6d5cdf
Diego Cuevas <diegocuevas579@gmail.com>
2019-11-11 17:44:54 -0500

    Login

---
+++ app/javascript/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { logout } from "../services/user";
+import { navigate } from "@reach/router";
+
+function Navbar() {
+  return (
+    <>
+      <button
+        onClick={() => {
+          logout();
+          navigate("/login");
+        }}
+      >
+        Logout
+      </button>
+      <hr />
+    </>
+  );
+}
+export default Navbar;
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :orders
+  has_secure_password
+
+ 
+
+  def self.valid_login?(email, password)
+    user = find_by(email: email)
+    user if user&.authenticate(password)
+  end
 end
